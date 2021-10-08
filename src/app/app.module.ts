691633fe5b652805a5fc0c261bf1064acf6f23a8
jpbinith <jpbinith@gmail.com>
2021-10-08 13:30:53 +0530

    before developing UIs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
